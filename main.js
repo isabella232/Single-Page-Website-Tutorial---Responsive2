e6fc7da9fe498522df88057a676c38e18b94b63b
Live Blogger <61611286+livebloggerofficial@users.noreply.github.com>
2020-04-28 15:48:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector(".menu-btn");
+const mobileMenuContainer = document.querySelector(".mobile-menu-container");
+const mobileMenuItems = document.querySelectorAll(
+  ".mobile-menu-container ul li a"
+);
+
+menuBtn.addEventListener("click", function () {
+  mobileMenuContainer.classList.toggle("active");
+});
+
+mobileMenuItems.forEach((i) => {
+  i.addEventListener("click", function () {
+    mobileMenuContainer.classList.remove("active");
+  });
+});
